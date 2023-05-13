f210f0186b731eda5cf03ed6d490743ba6e7b05b
mlobf <marcoslemeborbafilho@gmail.com>
2023-05-13 12:02:30 -0300

    login

--- user_micro/app.py
+++ user_micro/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask.sessions import SecureCookieSessionInterface
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 import models
 from routes import user_blueprint
 import os
@@ -14,8 +17,26 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + file_path
 
 models.init_app(app)
 app.register_blueprint(user_blueprint)
-
+login_manager = LoginManager(app)
 migrate = Migrate(app, models.db)
+
+
+@login_manager.request_loader
+def load_user_from_request(request):
+    api_key = request.headers.get("Authorization")
+    if api_key:
+        api_key = api_key.replace("Basic ", "", 1)
+        user = models.User.query.filter_by(api_key=api_key).first()
+        if user:
+            return user
+    return None
+
+
+class CustomSessionInterface(SecureCookieSessionInterface):
+    def save_session(self, *args, **kwargs):
+        if g.get("login_via_header"):
+            return
+        return super(CustomSessionInterface, self).save_session(*args, **kwargs)
 
 
 if __name__ == "__main__":
--- user_micro/routes.py
+++ user_micro/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, make_response
 from models import db, User
+from flask_login import login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 user_blueprint = Blueprint("user_api_routes", __name__, url_prefix="/api/user")
@@ -8,7 +9,7 @@ user_blueprint = Blueprint("user_api_routes", __name__, url_prefix="/api/user")
 @user_blueprint.route("/all", methods=["GET"])
 def get_all_users():
     all_users = User.query.all()
-    result = [user.serialize() for user in all_users]
+    result = [user.serializer() for user in all_users]
     message = {"message": "Returning all users", "result": result}
 
     return jsonify(message)
@@ -34,3 +35,26 @@ def create_user():
         response = {"message": f"Error has occured while saving a new user, ==> {e}"}
 
     return jsonify(response)
+
+
+@user_blueprint.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        response = {"message": "username does not exist"}
+        return make_response(jsonify(response, 401))
+
+    if check_password_hash(user.password, password):
+        user.update_api_key()
+        db.session.commit()
+        login_user(user)
+        response = {"message": "logged in ", "api_key": user.api_key}
+
+        return make_response(jsonify(response, 200))
+
+    response = {"message": "Access Denied"}
+    return make_response(jsonify(response, 401))
