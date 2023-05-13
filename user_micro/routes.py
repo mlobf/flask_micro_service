1c2c1f703be08f0956ecf412d1b4909478a09c54
mlobf <marcoslemeborbafilho@gmail.com>
2023-05-13 02:18:47 -0300

    Creating Users

--- user_micro/routes.py
+++ user_micro/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 user_blueprint = Blueprint("user_api_routes", __name__, url_prefix="/api/user")
 
@@ -11,3 +12,25 @@ def get_all_users():
     message = {"message": "Returning all users", "result": result}
 
     return jsonify(message)
+
+
+@user_blueprint.route("/create", methods=["POST"])
+def create_user():
+    try:
+        import pdb
+
+        user = User()
+        rq = request.get_json()
+
+        user.username = rq["username"]
+        user.password = generate_password_hash(rq["password"], method="sha256")
+        user.is_admin = True
+        db.session.add(user)
+        db.session.commit()
+
+        response = {"Message": "User created sucessfully", "result": user.serializer()}
+    except Exception as e:
+        print(str(e))
+        response = {"message": f"Error has occured while saving a new user, ==> {e}"}
+
+    return jsonify(response)
