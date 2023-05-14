8e43f02bf53f485812de8cdc87f48cc04e9b627a
mlobf <marcoslemeborbafilho@gmail.com>
2023-05-14 01:12:47 -0300

    Bugfix - Foi alterado o app de acordo com modelo do flask-login

--- user_micro/app.py
+++ user_micro/app.py
@@ -20,23 +20,31 @@ app.register_blueprint(user_blueprint)
 login_manager = LoginManager(app)
 migrate = Migrate(app, models.db)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.filter_by(id=user_id).first()
+
 
 @login_manager.request_loader
 def load_user_from_request(request):
-    api_key = request.headers.get("Authorization")
+    api_key = request.headers.get('Authorization')
     if api_key:
-        api_key = api_key.replace("Basic ", "", 1)
+        api_key = api_key.replace('Basic ', '', 1)
         user = models.User.query.filter_by(api_key=api_key).first()
         if user:
             return user
+
     return None
 
 
 class CustomSessionInterface(SecureCookieSessionInterface):
+    """Prevent creating session from API requests."""
+
     def save_session(self, *args, **kwargs):
-        if g.get("login_via_header"):
+        if g.get('login_via_header'):
             return
         return super(CustomSessionInterface, self).save_session(*args, **kwargs)
+
 
 
 if __name__ == "__main__":
--- user_micro/routes.py
+++ user_micro/routes.py
@@ -37,8 +37,38 @@ def create_user():
     return jsonify(response)
 
 
-@user_blueprint.route("/login", methods=["POST"])
+
+
+
+
+
+
+
+
+@user_blueprint.route('/login', methods=['POST'])
 def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        response = {'message': 'username does not exists'}
+        return make_response(jsonify(response), 401)
+    if check_password_hash(user.password, password):
+        user.update_api_key()
+        db.session.commit()
+        login_user(user)
+        response = {'message': 'logged in ', 'api_key': user.api_key}
+        return make_response(jsonify(response), 200)
+
+    response = {'message': 'Access denied'}
+    return make_response(jsonify(response), 401)
+
+
+# O problema esta aqui!
+@user_blueprint.route("/login1", methods=["POST"])
+def login1():
+    # import pdb;pdb.set_trace()
     username = request.form["username"]
     password = request.form["password"]
 
@@ -50,8 +80,8 @@ def login():
 
     if check_password_hash(user.password, password):
         user.update_api_key()
-        db.session.commit()
         login_user(user)
+        db.session.commit()
         response = {"message": "logged in ", "api_key": user.api_key}
 
         return make_response(jsonify(response, 200))
@@ -62,6 +92,7 @@ def login():
 
 @user_blueprint.route("/logout", methods=["POST"])
 def logout():
+    # import pdb;pdb.set_trace()
     if current_user.is_authenticated:
         logout_user()
         response = {"message": "User logout"}
@@ -71,19 +102,21 @@ def logout():
     return make_response(jsonify(response, 401))
 
 
+# Funciona, lembrar da porra da notation < nomedofulane >
 @user_blueprint.route("/<username>/exists", methods=["GET"])
 def user_exists(username):
+    # import pdb;pdb.set_trace()
     user = User.query.filter_by(username=username).first()
     if user:
         response = {"result": True}
         return make_response(jsonify(response, 200))
 
-    response = {"result": True}
+    response = {"result": False}
     return make_response(jsonify(response, 404))
 
 
-@user_blueprint.route("/", methods=["GET"])
-def get_current_user():
+@user_blueprint.route("/1", methods=["GET"])
+def get_current_user1():
     import pdb
 
     pdb.set_trace()
@@ -93,3 +126,12 @@ def get_current_user():
     else:
         response = {"message": "User not logged in"}
         return make_response(jsonify(response, 404))
+
+
+# teste
+@user_blueprint.route('/', methods=['GET'])
+def get_current_user():
+    if current_user.is_authenticated:
+        return jsonify({'result': current_user.serializer()}), 200
+    else:
+        return jsonify({'message': "User not logged in"}), 401
